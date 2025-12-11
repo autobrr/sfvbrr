5478438033a8b86b0a871fe761a3fa4b12b2c42f
Lucian Maly <lucian@redhat.com>
2025-12-11 19:42:39 +1100

    test(validate): Added recursive option for testing
    Signed-off-by: Lucian Maly <lucian@redhat.com>

--- test/run_validation_tests.sh
+++ test/run_validation_tests.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Validate one by one
 for test in $(ls -1 validate/); do cd validate/${test}/; sfvbrr validate $(ls -1); cd ../..; done > testresult.log
+
+# Validate recursively
+# sfvbrr validate -r validate/ > testresult.log
